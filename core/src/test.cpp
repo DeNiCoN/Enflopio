b7e0c4ef76955edfb10a3ad2bf807ffea7ae6bef
DeNiCoN <denicon1234@gmail.com>
2021-10-17 10:31:10 +0000

    Delete unused files

--- core/src/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "test.hpp"
-
-namespace Enflopio
-{
-    void test()
-    {
-        std::cout << "Hello, World!\n";
-    }
-}
--- core/src/test.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-namespace Enflopio
-{
-  void test();
-}
